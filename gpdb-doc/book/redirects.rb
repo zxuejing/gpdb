0fbc08ef7e93a4af732fcf62886dc63db471c758
dyozie <dyozie@pivotal.io>
2017-10-25 09:50:34 -0700

    Docs: Updating book build to show vers. 5.10

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/docs/600/common/gpdb-features.html'
-r301 '/index.html', '/docs/600/common/gpdb-features.html'
-r301 '/docs/600/index.html', '/docs/600/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/docs/600/common/gpdb-features.html'
+r301 '/', '/docs/510/common/gpdb-features.html'
+r301 '/index.html', '/docs/510/common/gpdb-features.html'
+r301 '/docs/510/index.html', '/docs/510/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/docs/510/common/gpdb-features.html'

5c7550cee096ccf60a0cd0748ea61980acaa14bc
Nadeem Ghani <nghani@pivotal.io>
2017-11-14 17:01:17 -0800

    gparray: fix constructor setup
    ~~This class was handling the hasMirrors field incorrectly. Now we're
    correctly setting the hasMirrors flag.~~
    
    This change broke the gpaddmirrors test, so fixed it.
    
    (cherry-picked from 5dfb98149af19775b19f815085810b613019d786)
    
    Edited this upon backporting to 5X_STABLE to use Fault Strategy to
    determine if the cluster has mirrors. This should now provide a fault
    strategy attribute on `gpArray`s that weren't initialized with
    initFromCatalog()

--- gpMgmt/bin/gppylib/gparray.py
+++ gpMgmt/bin/gppylib/gparray.py
@@ -1209,6 +1209,8 @@ class GpArray:
 
             # Handle regular segments
             elif segdb.isSegmentQE():
+                if segdb.isSegmentMirror():
+                    self.__strategy = FAULT_STRATEGY_FILE_REPLICATION
                 self.addSegmentDb(segdb)
 
             else:
@@ -1455,7 +1457,7 @@ class GpArray:
         array = GpArray(segments, origSegments, strategy)
         array.__version = version
         array.recoveredSegmentDbids = recoveredSegmentDbids
-        array.setFaultStrategy(strategy)
+        array.setFaultStrategy(strategy) # override the preliminary default `__strategy` with the database state, if available
         array.setFilespaces(filespaceArr)
 
         return array

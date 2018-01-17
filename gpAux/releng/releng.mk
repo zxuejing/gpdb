668720be85f00ef5e88c9ff4d211599f62caff00
Haisheng Yuan <hyuan@pivotal.io>
2018-01-17 12:41:36 -0600

    Change Orca version to 2.53.10

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -117,11 +117,10 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 	 (umask 002; ANT_OPTS="-Djavax.net.ssl.trustStore=$(BLD_TOP)/releng/make/dependencies/cacerts" \
 	/opt/releng/apache-ant/bin/ant -DBLD_ARCH=$(BLD_ARCH) \
 	-Divyrepo.host=$(IVYREPO_HOST) -Divyrepo.realm="$(IVYREPO_REALM)" \
-	-Divyrepo.user=$(IVYREPO_USER) -Divyrepo.passwd="$(IVYREPO_PASSWD)" resolve);
-	@echo "Resolve finished";
+	-Divyrepo.user=$(IVYREPO_USER) -Divyrepo.passwd="$(IVYREPO_PASSWD)" -quiet resolve);
 
 ifeq "$(findstring aix,$(BLD_ARCH))" ""
-	LD_LIBRARY_PATH='' wget -O - https://github.com/greenplum-db/gporca/releases/download/v2.53.11/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
+	LD_LIBRARY_PATH='' wget -q -O - https://github.com/greenplum-db/gporca/releases/download/v2.53.10/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
 endif
 
 clean_tools: opt_write_test

ddedcbedb50ce193aef9785b6d56393a7764d364
Taylor Vesely <tvesely@pivotal.io>
2018-04-19 11:47:52 -0600

    Restore timezone selection on GPDB startup
    Commit b749790 backports changes from Postgres 10.1 to parse the most
    recent version of the IANA timezone library format, but these changes
    also included some binary incompatible changes in behavior.
    Specifically, pg_timezone_pre_initialize() and identify_system_timezone()
    were removed, resulting in the timezone defaulting to 'GMT' on database
    startup, even if the TimeZone GUC was specifically set in
    postgresql.conf.
    
    This commit restores the previous behavior without reverting the IANA
    timezone library changes.

--- src/include/pgtime.h
+++ src/include/pgtime.h
@@ -76,13 +76,7 @@ extern void pg_tzenumerate_end(pg_tzenum *dir);
 
 extern pg_tz *session_timezone;
 extern pg_tz *log_timezone;
-/*
- * GPDB_92_MERGE_FIXME: Remove when commit ca4af308c32 has been merged.
- * gmt_timezone has been made obsolete with log_timezone being set by
- * pg_timezone_initialize(), but in order to avoid merge conflicts in
- * the code the callers are still allowed to use gmt_timezone.
- */
-#define gmt_timezone log_timezone
+extern pg_tz *gmt_timezone;
 
 /*
  * GPDB_92_MERGE_FIXME: Remove when commit ca4af308c32 is merged. This

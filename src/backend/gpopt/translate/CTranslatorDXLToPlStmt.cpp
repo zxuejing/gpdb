1fe322f8aba2a86f3efe08e5727d60c3add34a15
Dhanashree Kashid, Sambitesh Dash and Shreedhar Hardikar <pair+dkashid+sdash+shreedhar@pivotal.io>
2018-05-18 18:13:35 -0700

    Revert "Do not translate printable filters of PartitionSelector"
    This commit contains some updates to partition_pruning_optimizer answer
    file which causes some weird failures in CI but passes locally.
    
    We will fix the answer file and bring this commit back soon.
    
    This reverts commit 1a8e91c5e3ab0e26fe92acb224fd7b12f5b42dc9.

--- src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp
+++ src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp
@@ -3329,6 +3329,7 @@ CTranslatorDXLToPlStmt::PplanPartitionSelector
 
 	CDXLNode *pdxlnChild = NULL;
 	DrgPdxltrctx *pdrgpdxltrctx = GPOS_NEW(m_pmp) DrgPdxltrctx(m_pmp);
+	DrgPdxltrctx *pdrgpdxltrctxWithSiblings = GPOS_NEW(m_pmp) DrgPdxltrctx(m_pmp);
 
 	CDXLTranslateContext dxltrctxChild(m_pmp, false, pdxltrctxOut->PhmColParam());
 
@@ -3346,12 +3347,20 @@ CTranslatorDXLToPlStmt::PplanPartitionSelector
 	}
 
 	pdrgpdxltrctx->Append(&dxltrctxChild);
+	pdrgpdxltrctx->Append(pdxltrctxOut);
+
+	pdrgpdxltrctxWithSiblings->AppendArray(pdrgpdxltrctx);
+	if (NULL != pdrgpdxltrctxPrevSiblings)
+	{
+		pdrgpdxltrctxWithSiblings->AppendArray(pdrgpdxltrctxPrevSiblings);
+	}
 
 	CDXLNode *pdxlnPrL = (*pdxlnPartitionSelector)[EdxlpsIndexProjList];
 	CDXLNode *pdxlnEqFilters = (*pdxlnPartitionSelector)[EdxlpsIndexEqFilters];
 	CDXLNode *pdxlnFilters = (*pdxlnPartitionSelector)[EdxlpsIndexFilters];
 	CDXLNode *pdxlnResidualFilter = (*pdxlnPartitionSelector)[EdxlpsIndexResidualFilter];
 	CDXLNode *pdxlnPropExpr = (*pdxlnPartitionSelector)[EdxlpsIndexPropExpr];
+	CDXLNode *pdxlnPrintableFilter = (*pdxlnPartitionSelector)[EdxlpsIndexPrintableFilter];
 
 	// translate proj list
 	pplan->targetlist = PlTargetListFromProjList(pdxlnPrL, NULL /*pdxltrctxbt*/, pdrgpdxltrctx, pdxltrctxOut);
@@ -3364,7 +3373,7 @@ CTranslatorDXLToPlStmt::PplanPartitionSelector
 	ppartsel->levelExpressions = PlFilterList(pdxlnFilters, NULL /*pdxltrctxbt*/, pdrgpdxltrctx, pdxltrctxOut);
 
 	//translate residual filter
-	CMappingColIdVarPlStmt mapcidvarplstmt = CMappingColIdVarPlStmt(m_pmp, NULL /*pdxltrctxbt*/, pdrgpdxltrctx, pdxltrctxOut, m_pctxdxltoplstmt);
+	CMappingColIdVarPlStmt mapcidvarplstmt = CMappingColIdVarPlStmt(m_pmp, NULL /*pdxltrctxbt*/, pdrgpdxltrctxWithSiblings, pdxltrctxOut, m_pctxdxltoplstmt);
 	if (!m_pdxlsctranslator->FConstTrue(pdxlnResidualFilter, m_pmda))
 	{
 		ppartsel->residualPredicate = (Node *) m_pdxlsctranslator->PexprFromDXLNodeScalar(pdxlnResidualFilter, &mapcidvarplstmt);
@@ -3376,7 +3385,11 @@ CTranslatorDXLToPlStmt::PplanPartitionSelector
 		ppartsel->propagationExpression = (Node *) m_pdxlsctranslator->PexprFromDXLNodeScalar(pdxlnPropExpr, &mapcidvarplstmt);
 	}
 
-	// no need to translate printable filter - since it is not needed by the executor
+	//translate printable filter
+	if (!m_pdxlsctranslator->FConstTrue(pdxlnPrintableFilter, m_pmda))
+	{
+		ppartsel->printablePredicate = (Node *) m_pdxlsctranslator->PexprFromDXLNodeScalar(pdxlnPrintableFilter, &mapcidvarplstmt);
+	}
 
 	ppartsel->staticPartOids = NIL;
 	ppartsel->staticScanIds = NIL;
@@ -3403,6 +3416,7 @@ CTranslatorDXLToPlStmt::PplanPartitionSelector
 
 	// cleanup
 	pdrgpdxltrctx->Release();
+	pdrgpdxltrctxWithSiblings->Release();
 
 	return (Plan *) ppartsel;
 }

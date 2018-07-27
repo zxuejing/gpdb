ce26a63cfc786c2036d8d231331ff9905684dbfa
Asim R P <apraveen@pivotal.io>
2018-07-26 18:40:36 -0700

    Do not set log level to debug5 in ICW test
    The qp_executor test was doing this and it is not clear why.  The
    problem with debug5 is it enables log messages within fault injectors
    that are coded in inappropriate places.  When concurrent tests are run,
    this results in stuck spinlock PANICs as faultinjector API starts
    logging messages while holding a spinlock!  The fault injector API
    definitely needs to be fixed but until then, let this stopgap be there.

--- src/test/regress/sql/qp_executor.sql
+++ src/test/regress/sql/qp_executor.sql
@@ -2,7 +2,6 @@
 drop table if exists cf_executor_test;
 create table cf_executor_test (a integer);
 insert into cf_executor_test select a from generate_series(1,100) a;
-set log_min_messages=debug5;
 --end_ignore
 
 set debug_print_slice_table=on;
@@ -23,7 +22,6 @@ select * from cf_executor_test limit null;
 select * from cf_executor_test limit 0;
 
 --start_ignore
-reset log_min_messages;
 reset debug_print_slice_table;
 drop table cf_executor_test;
 --end_ignore

b94afa1336752ecbbb49f9a02913cbd3bd2dc15b
Ning Yu <nyu@pivotal.io>
2018-08-24 12:19:56 +0800

    Fix flaky fault injection in test pg_terminate_backend.
    In 00da6bbf0c we added a test case in pg_terminate_backend, however as
    it uses fault injection it will not work stably in a parallel test
    greoup.
    
    Moved this test case to a separate test group.
    
    (cherry picked from commit 226159cf4fe68cd1ea46c8504efe594ef3adc453)

--- src/test/isolation2/sql/pg_terminate_backend.sql
+++ src/test/isolation2/sql/pg_terminate_backend.sql
@@ -12,27 +12,3 @@ FROM pg_stat_activity WHERE current_query like 'create temp table t as select%' 
 
 -- query backend to ensure no PANIC on postmaster
 select count(*) from foo;
-
---
--- SIGSEGV issue when freeing gangs
---
-
-CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
-
-DROP TABLE IF EXISTS foo;
-CREATE TABLE foo (c1 int, c2 int) DISTRIBUTED BY (c1);
-
-10: BEGIN;
-
-SELECT gp_inject_fault('create_gang_in_progress', 'reset', 1);
-SELECT gp_inject_fault('create_gang_in_progress', 'suspend', 1);
-
-10&: SELECT * FROM foo a JOIN foo b USING (c2);
-
-SELECT pg_terminate_backend(procpid) FROM pg_stat_activity
- WHERE current_query = 'SELECT * FROM foo a JOIN foo b USING (c2);';
-
-SELECT gp_inject_fault('create_gang_in_progress', 'resume', 1);
-
-10<:
-10q

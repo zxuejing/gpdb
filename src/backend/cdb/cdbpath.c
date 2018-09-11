b53fb9fdfab67f08328521c98e09b2169c670e62
Joao Pereira <jdealmeidapereira@pivotal.io>
2018-09-11 10:04:38 -0400

    Revert "Make planner generate redistribute-motion."
    This reverts commit 67fb52e68d69c642f88407ac24443e966dd3d7e5.
    CI was failing and the problem was in a new test created in this commit
    that was expecting ORCA to do a Table Scan but in 5X with version 2.70.2
    it is doing a Seq Scan.
    
    This need to be reviewed before it is committed.

--- src/backend/cdb/cdbpath.c
+++ src/backend/cdb/cdbpath.c
@@ -656,22 +656,12 @@ cdbpath_partkeys_from_preds(PlannerInfo    *root,
         if (!b_partkey && rinfo->left_ec == rinfo->right_ec)
         {
 			ListCell *i;
-			bool found = false;
 
 			foreach(i, a_partkey)
 			{
 				PathKey *pathkey = (PathKey *) lfirst(i);
 				if (pathkey->pk_eclass == rinfo->left_ec)
-				{
-					found = true;
-					break;
-				}
-			}
-
-			if (!found)
-			{
-				PathKey    *a_pk = makePathKeyForEC(rinfo->left_ec);
-				a_partkey = lappend(a_partkey, a_pk);
+					a_partkey = lappend(a_partkey, rinfo->left_ec);
 			}
         }
 
@@ -923,10 +913,7 @@ cdbpath_motion_for_join(PlannerInfo    *root,
 
         /* If the bottlenecked rel can't be moved, bring the other rel to it. */
         if (single_immovable)
-		{
-			Assert(!other_immovable);
-			other->move_to = single->locus;
-		}
+            other->move_to = single->locus;
 
         /* Redistribute single rel if joining on other rel's partitioning key */
         else if (cdbpath_match_preds_to_partkey(root,
@@ -941,7 +928,7 @@ cdbpath_motion_for_join(PlannerInfo    *root,
             CdbPathLocus_MakeReplicated(&single->move_to);
 
         /* Redistribute both rels on equijoin cols. */
-        else if (!other_immovable &&
+        else if (!other->require_existing_order &&
                  cdbpath_partkeys_from_preds(root,
                                              redistribution_clauses,
                                              single->path,
@@ -1036,9 +1023,7 @@ cdbpath_motion_for_join(PlannerInfo    *root,
 
         /* Redistribute both rels on equijoin cols. */
         else if (!small->require_existing_order &&
-				 !small->has_wts &&
                  !large->require_existing_order &&
-				 !large->has_wts &&
                  cdbpath_partkeys_from_preds(root,
                                              redistribution_clauses,
                                              large->path,

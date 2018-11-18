c2f693f3375fd38a67f64005fdbaaac36cb8e612
lambgong <gong.gsheng@qq.com>
2018-11-18 19:08:28 +0800

    Remove incorrect fclose in gp_read_backup_file__ nd gp_write_backup_file__
    f is open by fd.c:AllocateFile, fd.c will automatically close all files
    opened with AllocateFile at transaction commit or abort, so there is no
    necessary to call fclose(f) here. Otherwise it will cause double free.

--- src/backend/cdb/cdbbackup.c
+++ src/backend/cdb/cdbbackup.c
@@ -1165,7 +1165,6 @@ gp_read_backup_file__(PG_FUNCTION_ARGS)
 
 	if (info.st_size != fread(pszFullStatus, 1, info.st_size, f))
 	{
-		fclose(f);
 		ereport(ERROR,
 				(errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION),
 				 errmsg("Error reading Backup File %s Type %d", pszFileName, fileType)));
@@ -1249,7 +1248,6 @@ gp_write_backup_file__(PG_FUNCTION_ARGS)
 	nBytes = strlen(pszBackup);
 	if (nBytes != fwrite(pszBackup, 1, nBytes, f))
 	{
-		fclose(f);
 		ereport(ERROR,
 				(errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION),
 				 errmsg("Error writing Backup File %s", pszFileName)));

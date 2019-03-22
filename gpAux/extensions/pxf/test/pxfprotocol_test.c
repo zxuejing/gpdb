239cf6158d997714ee4d379e4caccfd0cb4853b1
Francisco Guerrero <aguerrero@pivotal.io>
2019-03-22 09:59:51 -0700

    Remove XID and segmentID from the write path sent to PXF
    - The transaction ID and segment ID are only used by a subset of
      profiles. PXF should be able to append the transaction and segment IDs
      depending on the profile using the header information.
    - Remove write_file_name, do not send path

--- gpAux/extensions/pxf/test/pxfprotocol_test.c
+++ gpAux/extensions/pxf/test/pxfprotocol_test.c
@@ -139,7 +139,6 @@ test_pxfprotocol_import_first_call(void **state)
 	/* uri has been initialized */
 	assert_true(context->uri.data != NULL);
 	/* no write file name for import case */
-	assert_int_equal(context->write_file_name.len, 0);
 	assert_true(context->relation != NULL);
 	/* relation pointer is copied */
 	assert_int_equal(context->relation, relation);
@@ -205,7 +204,6 @@ test_pxfprotocol_import_last_call(void **state)
 
 	/* init data in context that will be cleaned up */
 	initStringInfo(&call_context->uri);
-	initStringInfo(&call_context->write_file_name);
 
 	/* set mock behavior for bridge cleanup */
 	expect_value(gpbridge_cleanup, context, call_context);
@@ -276,7 +274,6 @@ test_pxfprotocol_export_first_call(void **state)
 	/* uri has been initialized */
 	assert_true(context->uri.data != NULL);
 	/* write file name initialized, but empty, since it is filled by another component */
-	assert_int_equal(context->write_file_name.len, 0);
 	assert_true(context->relation != NULL);
 	/* relation pointer is copied */
 	assert_int_equal(context->relation, relation);
@@ -342,7 +339,6 @@ test_pxfprotocol_export_last_call(void **state)
 
 	/* init data in context that will be cleaned up */
 	initStringInfo(&call_context->uri);
-	initStringInfo(&call_context->write_file_name);
 
 	/* set mock behavior for bridge cleanup */
 	expect_value(gpbridge_cleanup, context, call_context);

4385e9e37138f4792d49b2cddd3eb8c82ecfd5f9
Jamie McAtamney <jmcatamney@pivotal.io>
2019-04-23 15:53:11 -0700

    Display report contents in Concourse output

--- concourse/scripts/compare_abi_ubuntu.bash
+++ concourse/scripts/compare_abi_ubuntu.bash
@@ -3,8 +3,6 @@
 set -o pipefail
 set -o errexit
 set -o nounset
-set -x
-
 if [ $# -lt 1 ]; then
     echo "Usage: $0 binary [binary ...]"
     exit 1
@@ -38,7 +36,7 @@ make -C abi-compliance-checker install prefix=/usr/local
 pushd abi
     echo "Comparing ABI between $old and $new..."
 
-    failed=
+    failed_binaries=()
     for binary in "$@"; do
         echo "Checking $binary ABI..."
         binary_name=$(basename "$binary")
@@ -46,7 +44,7 @@ pushd abi
         abi-dumper -lver "$old" -o "$binary_name.$old.dump" "${OLD_DIR}/$binary"
         abi-dumper -lver "$new" -o "$binary_name.$new.dump" "${NEW_DIR}/$binary"
         if ! abi-compliance-checker -l "$binary_name" -old "$binary_name.$old.dump" -new "$binary_name.$new.dump"; then
-            failed+="$binary "
+            failed_binaries+=("$binary")
         fi
     done
 
@@ -54,12 +52,16 @@ pushd abi
     find . -name compat_report.html -exec sh -c 'html2text {} > `dirname {}`/compat_report.txt' \;
 popd
 
-if [ -n "$failed" ]; then
-    set +x
+if [ -n "$failed_binaries" ]; then
     echo
     echo "The following binaries have ABI differences from $old:"
     echo
-    echo "    $failed"
+    echo "    ${failed_binaries[*]}"
     echo
-    echo "A report has been generated."
+    echo "A report has been generated, and is displayed below:"
+    for failed_binary in ${failed_binaries[@]}; do
+        # directory structure is /[path to reports]/[binary name]/[old version]_to_[new version]
+        report_path="./abi/compat_reports/$(basename $failed_binary)/${old}_to_${new}/compat_report.txt"
+        cat $report_path
+    done
 fi

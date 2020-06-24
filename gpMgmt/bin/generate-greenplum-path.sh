2eee1d1e6b83b95141565af621040d1d264cc866
Tingfang Bao <bbao@pivotal.io>
2020-06-24 21:11:46 +0000

    Update generate-greenplum-path.sh for upgrade package
    Following the [Greenplum Server RPM Packaging Specification][0], we need
    to update greenplum_path.sh file, and ensure many environment variables
    set correct.
    
    There are a few basic requirments for Greenplum Path Layer:
    
    * greenplum-path.sh shall be installed to `${installation
      prefix}/greenplum-db-[package-version]/greenplum_path.sh`
    * ${GPHOME} is set by given parameter, by default it should point to
      `%{installation prefix}/greenplum-db-devel`
    * `${LD_LIBRARY_PATH}` shall be safely set to avoid a trailing colon
      (which will cause the linker to search the current directory when
      resolving shared objects)
    * `${PYTHONHOME}` shall be set to `${GPHOME}/ext/python`
    * `${PYTHONPATH}` shall be set to `${GPHOME}/lib/python`
    * `${PATH}` shall be set to `${GPHOME}/bin:${PYTHONHOME}/bin:${PATH}`
    * If the file `${GPHOME}/etc/openssl.cnf` exists then `${OPENSSL_CONF}`
      shall be set to `${GPHOME}/etc/openssl.cnf`
    * The greenplum_path.sh file shall pass [ShellCheck][1]
    
    [0]: https://github.com/greenplum-db/greenplum-database-release/blob/master/Greenplum-Server-RPM-Packaging-Specification.md#detailed-package-behavior
    [1]: https://github.com/koalaman/shellcheck
    
    [#171588834]
    
    Co-authored-by: Tingfang Bao <bbao@pivotal.io>
    Co-authored-by: Bradford D. Boyle <bradfordb@vmware.com>
    Co-authored-by: Xin Zhang <zhxin@vmware.com>

--- gpMgmt/bin/generate-greenplum-path.sh
+++ gpMgmt/bin/generate-greenplum-path.sh
@@ -1,93 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ x$1 != x ] ; then
-    GPHOME_PATH=$1
-else
-    GPHOME_PATH="\`pwd\`"
+if [ -z "$1" ]; then
+  printf "Must specify a value for GPHOME"
+  exit 1
 fi
 
-if [ "$2" = "ISO" ] ; then
-	cat <<-EOF
-		if [ "\${BASH_SOURCE:0:1}" == "/" ]
-		then
-		    GPHOME=\`dirname "\$BASH_SOURCE"\`
-		else
-		    GPHOME=\`pwd\`/\`dirname "\$BASH_SOURCE"\`
-		fi
-	EOF
-else
-	cat <<-EOF
-		GPHOME=${GPHOME_PATH}
-	EOF
-fi
-
-
-PLAT=`uname -s`
-if [ $? -ne 0 ] ; then
-    echo "Error executing uname -s"
-    exit 1
-fi
-
-cat << EOF
-
-# Replace with symlink path if it is present and correct
-if [ -h \${GPHOME}/../greenplum-db ]; then
-    GPHOME_BY_SYMLINK=\`(cd \${GPHOME}/../greenplum-db/ && pwd -P)\`
-    if [ x"\${GPHOME_BY_SYMLINK}" = x"\${GPHOME}" ]; then
-        GPHOME=\`(cd \${GPHOME}/../greenplum-db/ && pwd -L)\`/.
-    fi
-    unset GPHOME_BY_SYMLINK
-fi
-EOF
-
+GPHOME_PATH="$1"
 cat <<EOF
-#setup PYTHONHOME
-if [ -x \$GPHOME/ext/python/bin/python ]; then
-    PYTHONHOME="\$GPHOME/ext/python"
-fi
-EOF
+GPHOME="${GPHOME_PATH}"
 
-#setup PYTHONPATH
-if [ "x${PYTHONPATH}" == "x" ]; then
-    PYTHONPATH="\$GPHOME/lib/python"
-else
-    PYTHONPATH="\$GPHOME/lib/python:${PYTHONPATH}"
-fi
-cat <<EOF
-PYTHONPATH=${PYTHONPATH}
-EOF
-
-GP_BIN_PATH=\$GPHOME/bin
-GP_LIB_PATH=\$GPHOME/lib
-
-if [ -n "$PYTHONHOME" ]; then
-    GP_BIN_PATH=${GP_BIN_PATH}:\$PYTHONHOME/bin
-    GP_LIB_PATH=${GP_LIB_PATH}:\$PYTHONHOME/lib
-fi
-cat <<EOF
-PATH=${GP_BIN_PATH}:\$PATH
-EOF
-
-cat <<EOF
-LD_LIBRARY_PATH=${GP_LIB_PATH}:\${LD_LIBRARY_PATH-}
-export LD_LIBRARY_PATH
-EOF
+PYTHONHOME="${GPHOME}/ext/python"
+PYTHONPATH="${GPHOME}/lib/python"
+PATH="${GPHOME}/bin:${PYTHONHOME}/bin:${PATH}"
+LD_LIBRARY_PATH="${GPHOME}/lib:${PYTHONHOME}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 # openssl configuration file path
-cat <<EOF
-OPENSSL_CONF=\$GPHOME/etc/openssl.cnf
-EOF
+if [ -e "$GPHOME/etc/openssl.cnf" ]; then
+	OPENSSL_CONF="$GPHOME/etc/openssl.cnf"
+fi
 
-cat <<EOF
 export GPHOME
 export PATH
-EOF
-
-cat <<EOF
-export PYTHONPATH
 export PYTHONHOME
-EOF
-
-cat <<EOF
+export PYTHONPATH
+export LD_LIBRARY_PATH
 export OPENSSL_CONF
 EOF

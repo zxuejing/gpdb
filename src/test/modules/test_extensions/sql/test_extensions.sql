1825c136dba56f9c4945924e9520f6fa3416729e
Noah Misch <noah@leadboat.com>
2023-08-07 06:05:56 -0700

    Reject substituting extension schemas or owners matching ["$'\].
    Substituting such values in extension scripts facilitated SQL injection
    when @extowner@, @extschema@, or @extschema:...@ appeared inside a
    quoting construct (dollar quoting, '', or "").  No bundled extension was
    vulnerable.  Vulnerable uses do appear in a documentation example and in
    non-bundled extensions.  Hence, the attack prerequisite was an
    administrator having installed files of a vulnerable, trusted,
    non-bundled extension.  Subject to that prerequisite, this enabled an
    attacker having database-level CREATE privilege to execute arbitrary
    code as the bootstrap superuser.  By blocking this attack in the core
    server, there's no need to modify individual extensions.  Back-patch to
    v11 (all supported versions).
    
    Reported by Micah Gate, Valerie Woolard, Tim Carey-Smith, and Christoph
    Berg.
    
    Security: CVE-2023-39417
    (cherry picked from commit eb044d8f0aee1ba4950b0867f6ca9328374318db)

--- src/test/modules/test_extensions/sql/test_extensions.sql
+++ src/test/modules/test_extensions/sql/test_extensions.sql
@@ -1,18 +1,20 @@
+CREATE SCHEMA has$dollar;
+
 -- test some errors
 CREATE EXTENSION test_ext1;
 CREATE EXTENSION test_ext1 SCHEMA test_ext1;
 CREATE EXTENSION test_ext1 SCHEMA test_ext;
-CREATE SCHEMA test_ext;
-CREATE EXTENSION test_ext1 SCHEMA test_ext;
+CREATE EXTENSION test_ext1 SCHEMA has$dollar;
 
 -- finally success
-CREATE EXTENSION test_ext1 SCHEMA test_ext CASCADE;
+CREATE EXTENSION test_ext1 SCHEMA has$dollar CASCADE;
 
 SELECT extname, nspname, extversion, extrelocatable FROM pg_extension e, pg_namespace n WHERE extname LIKE 'test_ext%' AND e.extnamespace = n.oid ORDER BY 1;
 
 CREATE EXTENSION test_ext_cyclic1 CASCADE;
 
-DROP SCHEMA test_ext CASCADE;
+DROP SCHEMA has$dollar CASCADE;
+CREATE SCHEMA has$dollar;
 
 CREATE EXTENSION test_ext6;
 DROP EXTENSION test_ext6;
@@ -321,3 +323,10 @@ show search_path;
 
 reset search_path;
 drop schema issue6716 cascade;
+
+--
+-- Test @extschema@ syntax.
+--
+CREATE SCHEMA "has space";
+CREATE EXTENSION test_ext_extschema SCHEMA has$dollar;
+CREATE EXTENSION test_ext_extschema SCHEMA "has space";

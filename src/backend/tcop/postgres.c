8d998f1897edb22dec57823e4076717a6abfe524
zxuejing <zxuejing@vmware.com>
2023-09-13 00:11:08 -0700

    move parallel cursor timeout process from ProcessClientReadInterrupt to ProcessInterrupts

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -681,29 +681,6 @@ ProcessClientReadInterrupt(bool blocked)
 		/* Process notify interrupts, if any */
 		if (notifyInterruptPending)
 			ProcessNotifyInterrupt();
-		/*
-		 * issue: https://github.com/greenplum-db/gpdb/issues/15143
-		 * handle errors in parallel retrieve cursor
-		 */
-		if (GpParallelRetrieveCursorCheckPending)
-		{
-			int num;
-			GpParallelRetrieveCursorCheckPending = false;
-
-			Assert(Gp_role == GP_ROLE_DISPATCH);
-
-			/* Invoke cdbdisp_checkForCancel and do not raise any error */
-			gp_check_parallel_retrieve_cursor_error();
-
-			num = GetNumOfParallelRetrieveCursors();
-
-			/* Reset the alarm to check after some seconds */
-			if (num > 0)
-			{
-				elog(DEBUG1, "There are still %d parallel retrieve cursors alive", num);
-				enable_parallel_retrieve_cursor_timeout();
-			}
-		}
 	}
 	else if (ProcDiePending)
 	{
@@ -4105,6 +4082,32 @@ ProcessInterrupts(const char* filename, int lineno)
 			DisconnectAndDestroyUnusedQEs();
 
 		IdleGangTimeoutPending = false;
+	}
+
+	/*
+	 * issue: https://github.com/greenplum-db/gpdb/issues/15143
+	 * handle errors in parallel retrieve cursor
+	 * We process parallel cursor timeout When DoingCommandRead is true,
+	 * to prevent nest call of CheckDispatchResult.
+	 */
+	if (DoingCommandRead && GpParallelRetrieveCursorCheckPending)
+	{
+		int num;
+		GpParallelRetrieveCursorCheckPending = false;
+
+		Assert(Gp_role == GP_ROLE_DISPATCH);
+
+		/* Invoke cdbdisp_checkForCancel and do not raise any error */
+		gp_check_parallel_retrieve_cursor_error();
+
+		num = GetNumOfParallelRetrieveCursors();
+
+		/* Reset the alarm to check after some seconds */
+		if (num > 0)
+		{
+			elog(DEBUG1, "There are still %d parallel retrieve cursors alive", num);
+			enable_parallel_retrieve_cursor_timeout();
+		}
 	}
 
 	if (ParallelMessagePending)
